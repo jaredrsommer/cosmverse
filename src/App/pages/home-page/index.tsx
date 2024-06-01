e369df30bba1af7fa432b180bb66d0af5057e220
Ch1cK3N_t4c0s <51533784+jaredrsommer@users.noreply.github.com>
2024-05-31 20:06:29 -0400

    Add files via upload

--- src/App/pages/home-page/index.tsx
+++ src/App/pages/home-page/index.tsx
@@ -6,8 +6,8 @@ import { Box, Flex, Stack } from '@chakra-ui/react';
 import { FaDiscord, FaGithub, FaMedium, FaTelegram, FaTwitter } from 'react-icons/fa';
 
 import { SocialButton } from '../../components';
-import  cosmverseHomepage  from '../../assets/background-home2.jpg';
-import  cosmverseLogo  from '../../assets/cosmverse_logo.svg';
+import  cosmverseHomepage  from '../../assets/homepage.png';
+
 
 export const HomePage = () => {
   return (
@@ -19,27 +19,26 @@ export const HomePage = () => {
       <Box w='100vw' h='100vh' backgroundImage={cosmverseHomepage} zIndex='-1' position='relative' backgroundSize='cover'>
       </Box>
 
-      <Box w='150px' h='150px' backgroundImage={cosmverseLogo} zIndex='1' position='absolute' top='0vh' alignSelf={['center','center','flex-end','flex-end']} backgroundSize='cover'>
-      </Box>
+
 
 
 
         <Stack direction={["column", "row"]}  zIndex='1' position='absolute' bottom={['25vh', '10vh']}>
          
-          <SocialButton label={'Twitter'} href={'https://twitter.com/Cosmverse'} width='40px' height='40px' backgroundColor='white'>
+          <SocialButton label={'Twitter'} href={'https://twitter.com/Event__16'} width='40px' height='40px' backgroundColor='white'>
             <FaTwitter />
           </SocialButton>
 
-          <SocialButton label={'Medium'} href={'https://medium.com/@Cosmverse'} width='40px' height='40px' backgroundColor='white'>
+          <SocialButton label={'Medium'} href={'https://medium.com/@event_16'} width='40px' height='40px' backgroundColor='white'>
             <FaMedium />
           </SocialButton>
-          <SocialButton label={'Telegram'} href={'https://t.me/cosmverse_official'} width='40px' height='40px' backgroundColor='white'>
+          <SocialButton label={'Telegram'} href={'https://t.me/event_16'} width='40px' height='40px' backgroundColor='white'>
               <FaTelegram />
           </SocialButton>
-          <SocialButton label={'Github'} href={'https://github.com/CosmosContracts/cosmverse'} width='40px' height='40px' backgroundColor='white'>
+          <SocialButton label={'Github'} href={'https://github.com/Jaredrsommer/cosmverse'} width='40px' height='40px' backgroundColor='white'>
             <FaGithub />
           </SocialButton>
-          <SocialButton label={'Discord'} href={'https://discord.com/invite/b9wgpfzGx4'} width='40px' height='40px' backgroundColor='white'>
+          <SocialButton label={'Discord'} href={'https://discord.com/invite/IBC_GANG_BOI!'} width='40px' height='40px' backgroundColor='white'>
             <FaDiscord />
           </SocialButton>
 
