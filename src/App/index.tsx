422f52562f6368dd5e9d9f55f1818849b269f29c
Ch1cK3N_t4c0s <51533784+jaredrsommer@users.noreply.github.com>
2024-06-10 20:45:50 -0400

    Update index.tsx
    fix uploading issue

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -13,14 +13,9 @@ import {HomePage} from "./pages"
 import {Gallery} from "./pages"
 import {Create} from "./pages"
 import {Account} from "./pages"
-<<<<<<< HEAD
-import { SdkProvider } from "./services/client/wallet"
-import { config } from "../config";
-=======
 import {Landing} from './pages';
 import {SdkProvider} from "./services/client/wallet"
 import {config} from "../config";
->>>>>>> a079383 (Adding Initial Branding)
 import theme from "./theme"
 
 export const App = () => (
@@ -34,10 +29,7 @@ export const App = () => (
           <Route exact path="/gallery" component={Gallery} />
           <Route exact path="/create" component={Create} />
           <Route exact path="/account" component={Account} />
-<<<<<<< HEAD
-=======
           <Route exact path="/landing" component={Landing} />
->>>>>>> a079383 (Adding Initial Branding)
           {/* ... other routes */}
         </Switch>
 
